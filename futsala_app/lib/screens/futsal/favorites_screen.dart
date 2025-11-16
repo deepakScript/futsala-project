bdffd3624f34453a83378122ac7717ba5e1d8913
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-11-16 16:39:55 +0545

    feat(favorites): UI design favourites page

---
+++ futsala_app/lib/screens/futsal/favorites_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Favorites')),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.bookmark, size: 100, color: Colors.red),
+            SizedBox(height: 20),
+            Text('Favorites Page', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
