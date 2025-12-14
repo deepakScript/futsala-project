ba1d470762efdd790b5a43471ac207b8966de3bc
sabeenadk <adksabin779@gmail.com>
2025-12-14 19:26:56 +0545

    feat: update BookingPage and FavoritesPage constructors to use 'super.key' syntax; add launch.json for VSCode configuration

--- futsala_app/lib/screens/main/ScaffoldWithNavBar.dart
+++ futsala_app/lib/screens/main/ScaffoldWithNavBar.dart
@@ -5,7 +5,7 @@ import 'package:go_router/go_router.dart';
 class ScaffoldWithNavBar extends StatelessWidget {
   final Widget child;
 
-  const ScaffoldWithNavBar({Key? key, required this.child}) : super(key: key);
+  const ScaffoldWithNavBar({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
