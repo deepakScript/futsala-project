73a59fbdf30bfb5c73562a0065c4884d300a08ab
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2026-01-14 21:06:26 +0545

    fix(error): fix the placeholder of email where it's show the admin suggested email

--- futsala_superadmin/src/app/(auth)/page.tsx
+++ futsala_superadmin/src/app/(auth)/page.tsx
@@ -63,7 +63,7 @@ export default function LoginPage() {
                 <Input
                   id="email"
                   type="email"
-                  placeholder="admin@futsal.com"
+                  placeholder="Enter Email"
                   required
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
